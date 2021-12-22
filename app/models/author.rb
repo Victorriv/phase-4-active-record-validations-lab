c3b70a30e77f369df025eb49bb8b7d0b7593bacf
Victor <victorrivera714@yahoo.com>
2021-12-21 16:04:00 -0800

    commit tree 1bf09c5752f0959862560ee039016909ea7eae9f

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+
+    validates :name, presence: true, uniqueness: true #all authors have a name, no same name. 
+    validates :phone_number, length: {is:10} #phone number is exactly 10 digits
 end
